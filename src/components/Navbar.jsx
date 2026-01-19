d2d84a78033f596bd4403b71f8924c506c180c1d
Nirvan <joneja.nirvan@gmail.com>
2026-01-19 10:12:02 +0530

    final corrections

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             <span className="text-[#915EFF]">Nirvan</span>&nbsp;
             <span className="sm:block hidden text-secondary">
-              | JavaScript Mastery
+              | Work Portfolio
             </span>
           </p>
         </Link>
