1fbdaf1f5e45fb35853eb37cda05b49944254817
Nirvan <joneja.nirvan@gmail.com>
2026-05-02 00:28:30 +0530

    fix android white canvas alpha issue

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -52,8 +52,10 @@ const ComputersCanvas = () => {
         shadows
         dpr={[1, 1]}
         camera={{ position: [20, 3, 5], fov: 25 }}
-        gl={{ preserveDrawingBuffer: true, antialias: false }}
-        onCreated={({ gl }) => gl.setClearColor("#050816")}
+        gl={{ preserveDrawingBuffer: true, antialias: false, alpha: false }}
+        onCreated={({ gl }) => {
+          gl.setClearColor("#050816", 1);
+        }}
       >
         <Suspense fallback={<CanvasLoader />}>
           <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
